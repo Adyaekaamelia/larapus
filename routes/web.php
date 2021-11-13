feb634e92ed28bd3f567d317b3d6502248ce8bf4
Adya <adyka013@gmail.com>
2021-11-13 11:21:55 +0700

    Author and book

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,12 +25,17 @@ return view('layouts.admin');
 });
 
 Auth::routes();
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::group(['prefix'=>'admin', 'middleware'=>['auth','role:admin']], function (){
-    Route::get('/',function(){
-        return view('admin');
-    });
+
+Route::group(
+    ['prefix'=>'admin', 'middleware'=>['auth','role:admin']], function (){
+   Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
 
+Route::group(
+    ['prefix'=>'user', 'middleware'=>['auth','role:member']], function (){
+   Route::get('/sample', [App\Http\Controllers\HomeController::class, 'index2'])->name('home');
+});
 
+Route::resource('author', AuthorController::class);
+Route::resource('book', BookController::class);
